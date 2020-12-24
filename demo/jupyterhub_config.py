6131dc2ddddf7542c470b6649cd64d6c026e964a
Tom Augspurger <taugspurger@microsoft.com>
2020-12-24 14:34:41 -0600

    update config

--- demo/jupyterhub_config.py
+++ demo/jupyterhub_config.py
@@ -10,13 +10,16 @@ c.JupyterHub.services = [
         "admin": True,
         "url": "http://127.0.0.1:21211",
         "command": ["papermillhub"],
+        "api_token": "super-secret"
     }
 ]
 c.JupyterHub.service_tokens = {"super-secret": "papermillhub"}
 
 # Setup authenticator and spawners
 c.JupyterHub.admin_access = True  # Service needs to access user servers.
-c.JupyterHub.authenticator_class = "dummy"
-c.JupyterHub.spawner_class = "simple"
+c.JupyterHub.authenticator_class = 'jupyterhub.auth.DummyAuthenticator'
+
+c.JupyterHub.spawner_class = "jupyterhub.spawner.SimpleLocalProcessSpawner"
 c.SimpleLocalProcessSpawner.home_dir_template = os.getcwd()
 c.Spawner.default_url = "/lab"
+c.JupyterHub.cookie_secret_file = '/home/taugspurger/jupyterhub_cookie_secret'
